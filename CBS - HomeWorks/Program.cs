e39c6771b7f8b270c9714f1155be565ba15a68c8
Михайлюк Артур <mihajluk@ispro.ua>
2020-05-24 16:17:03 +0300

    Задание 3

--- CBS - HomeWorks/Program.cs
+++ CBS - HomeWorks/Program.cs
@@ -7,9 +7,26 @@ namespace CBS___HomeWorks
         static void Main(string[] args)
         {
             
+            /*---Задание 2---*/
             
+            Double pi = 3.141592653d;
+            Decimal e = 2.7182818284590452m;
+
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+
+            /*---Задание 3---*/
+
+            String str1 = "\nмоя строка 1";
+            String str2 = "\tмоя строка 2";
+            String str3 = "\aмоя строка 3";
             
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+
+
+
         }
     }
 }

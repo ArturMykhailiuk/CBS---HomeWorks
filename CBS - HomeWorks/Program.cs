0291e1f4c1962d05c843fad8bacc11a55f0ca142
Михайлюк Артур <mihajluk@ispro.ua>
2020-05-24 15:52:33 +0300

    Добавьте файлы проекта.

---
+++ CBS - HomeWorks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CBS___HomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CBS - Homework3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CBS___Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
